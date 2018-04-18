4c66e7ea87170074eb446efc02b70a4ebd915800
debrayona <debra.chacaliaz@unmsm.edu.pe>
2018-04-18 15:41:36 -0500

    more data

--- src/data/digimon.js
+++ src/data/digimon.js
@@ -1,5 +1,9 @@
     export const digimon = [
         {id: 1, name: 'Agumon', tipo: 'virus'},
-        {id: 2, name: 'Gagumon', tipo: 'vacuna'},
-        {id: 3, name: 'Guilmon ', tipo: 'virus'}
+        {id: 2, name: 'Gabumon', tipo: 'vacuna'},
+        {id: 3, name: 'Guilmon ', tipo: 'virus'},
+        {id: 4, name: 'Renamon ', tipo: 'data'},
+        {id: 5, name: 'Gatomon', tipo: 'vacuna'},
+        {id: 6 , name: 'Impmon', tipo: 'virus'},
+
     ]
